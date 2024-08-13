cf6c88d1a85023736f44a441a3efc4775f0a1c4a
aokishiori <102342763+aokishiori@users.noreply.github.com>
2024-08-13 20:59:25 +0900

    Update topo_opt_level_set_fluid_flow.py

--- problems/topo_opt_level_set_fluid_flow.py
+++ problems/topo_opt_level_set_fluid_flow.py
@@ -60,3 +60,62 @@ class TopologyOptimizationProblem:
             objective_function += lambda_char*(char_func_elem - level_set_elem_scaled)**2
 
         self.binary_quadratic_model = objective_function
+
+class TopologyOptimizationParameterTuning:
+
+    def __init__(self, n_elem, n_qubits_per_variable):
+        self.n_elem = n_elem
+        self.n_qubits_per_variable = n_qubits_per_variable
+
+    def generate_discretizaton(self):
+        
+        # Create qubits.
+        self.q = gen_symbols(BinaryPoly, self.n_elem, self.n_qubits_per_variable+1) # TODO +1 for ancilla qubit?
+
+    def generate_qubo_formulation(self, u, v, el, ge, hev, dc volume_fraction_max, neighbor_elements_Q1):
+        volume_max = volume_fraction_max * self.n_elem
+
+        # Coefficient for porous resistance.
+        coef_pr = 10**2/4.
+
+        # Coefficients for...
+        alpha_el = el*100.*4. # energy loss     🐳
+        alpha_eqn_c = 1. # equation constraint
+        alpha_ge = ge # gradient energy
+        alpha_hev = hev # heaviside function
+        #alpha_bc = 1.0 # boundary constraint
+        alpha_dc = dc # domain constraint
+
+        # Create qubits.
+        # q = gen_symbols(BinaryPoly, n_elem, n_qubits_per_variable+1)
+
+        # Initialize objective function.
+        objective_function = BinaryPoly()
+
+        # Add different contributions to objective function.
+        # Energy loss.
+        for t_id in range(self.n_elem):
+            coef_pr_e = (1-self.q[t_id][-1])*coef_pr
+            t_uvec = u[t_id]/xc*2
+            t_vvec = v[t_id]/xc*2
+            objective_function += alpha_el*(coef_pr_e*(t_uvec*t_uvec+t_vvec*t_vvec) )
+        # Gradient energy.  🐳
+        for t_id,t_q in enumerate(self.q):
+            for tt_id in neighbor_elements_Q1[t_id]:
+                for t_coord in range(2): # 2 if it's two-dimensional; in general, n if it's n-dimensional
+                    if len(tt_id) == 1:
+                        phi_i = (sum_poly(t_q[:-1])/self.n_qubits_per_variable*2)-1
+                        phi_j = (sum_poly(self.q[tt_id[0]][:-1])/self.n_qubits_per_variable*2)-1
+                        t_obj = alpha_ge/2*((phi_i-phi_j)/2)**2
+                    elif len(tt_id) == 2:
+                        phi_i = (sum_poly(self.q[tt_id[0]][:-1])/self.n_qubits_per_variable*2)-1
+                        phi_j = (sum_poly(self.q[tt_id[1]][:-1])/self.n_qubits_per_variable*2)-1
+                        t_obj = alpha_ge/2*(phi_i-phi_j)**2
+                    objective_function += t_obj
+        # Heaviside function.
+        for t_id, t_q in enumerate(self.q):
+            objective_function += alpha_hev*(sum_poly(t_q[:-1])/self.n_qubits_per_variable - t_q[-1])**2
+        # Domain constraints.
+        objective_function += alpha_dc*(sum_poly([t[-1] for t in self.q]) - volume_max)**2
+
+        self.binary_quadratic_model = objective_function
